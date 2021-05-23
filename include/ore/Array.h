519ee7ff59ecb86c90ae9060c2cc5c11699f3a46
Léo Lam <leo@leolam.fr>
2021-05-23 14:31:01 +0200

    ore: Add Array

---
+++ include/ore/Array.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+
+namespace ore {
+
+// This is like a std::span, not a fixed-size array like std::array.
+template <typename T>
+class Array {
+public:
+    Array() = default;
+
+    T* data() const { return m_data; }
+    int size() const { return m_size; }
+
+    auto begin() const { return data(); }
+    auto end() const { return data() + size(); }
+
+    void SetData(T* data, int size) {
+        m_data = data;
+        m_size = size;
+    }
+
+    void DestructElements() { std::destroy(begin(), end()); }
+
+private:
+    T* m_data;
+    int m_size;
+};
+
+}  // namespace ore
